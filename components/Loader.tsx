47b5d04bd64e56b1ea18ed870a54e2d84a7817e4
owayys <76042418+owayys@users.noreply.github.com>
2024-01-06 18:51:00 +0000

    CSS updates

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loader = () => {
   return (
     <View style={styles.overlay}>
-      <ActivityIndicator animating={true} size="large" color="blue" />
+      <ActivityIndicator animating={true} size="large" color="#35C2B0" />
     </View>
   );
 };
@@ -12,7 +12,7 @@ const Loader = () => {
 const styles = StyleSheet.create({
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
     zIndex: 1,
