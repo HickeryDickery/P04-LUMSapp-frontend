d5139bad90dbed62fa47d724c1ae034b64cbf376
owayys <76042418+owayys@users.noreply.github.com>
2024-01-07 21:28:11 +0000

    Updated Constant

--- constants/ip.ts
+++ constants/ip.ts
@@ -1,3 +1,1 @@
-// export const IP = "https://uninterested-frog-top-hat.cyclic.app/api";
-// export const IP = "http://192.168.1.10:8000/api";
-export const IP = "http://192.168.100.8:8000/api";
+export const IP = "https://uninterested-frog-top-hat.cyclic.app/api";
