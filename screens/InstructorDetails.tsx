8046dcda78970104debd0e48003e513286ac162c
Nauman <24100311@lums.edu.pk>
2024-02-04 17:14:35 +0500

    add-instructor-ratings

--- screens/InstructorDetails.tsx
+++ screens/InstructorDetails.tsx
@@ -1,20 +1,67 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Button } from "react-native-paper";
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import Loader from "../components/Loader";
 
+import StarRating from "../components/StarRating";
+
+// TO IMPORT
+const reviewsCount = 10;
+const zambeelRating = 3.88
+const profileDescription = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit quos dignissimos id qui eligendi facilis reprehenderit, repudiandae optio at consequatur, labore impedit earum fugiat ea ipsa laborum autem vero omnis?"
+// END TO IMPORT
+
+
+
+// tabview
+const DetailsTab = () => (
+  <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+    <View style={{ flex: 1}}>
+      <Text style={{color: "#35C2C1", fontSize: 15, fontWeight: "bold", padding: 20}}>Overall Rating</Text>
+    
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{paddingLeft: 20}}><StarRating initialValue={2} /></View>
+        <Text style={{ color: 'white', marginLeft: 5, marginTop: 10, fontSize: 10}}>({reviewsCount})</Text>
+        <Text  style={{ color: 'white', marginLeft: 100, fontSize: 25, fontWeight: "bold"}}>{zambeelRating}/5</Text>
+      </View>
+      <Text  style={{ color: '#047CD2', marginLeft: "80%", fontSize: 15}}>Zambeel</Text>
+     
+      <Text style={{color: "#35C2C1", fontSize: 15, fontWeight: "bold", padding: 20}}>Profile</Text>
+      <Text style={{color: "white", fontSize: 15, paddingHorizontal: 20}}>{profileDescription}</Text>
+    </View>
+  </ScrollView>
+);
+
+
+const ReviewsTab = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+
+const renderScene = SceneMap({
+  first: DetailsTab,
+  second: ReviewsTab,
+});
 
 const InstructorDetails = ({ route }) => {
   const navigation = useNavigation();
   const [loading, setLoading] = useState(false);
   const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
   const { name, school, department } = route.params;
+
+  //
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'Details' },
+    { key: 'second', title: 'Reviews' },
+  ]);
+
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.heading}>Instructor Is: {name} & {school} & {department}</Text> */}
       <Button
         onPress={() => { navigation.goBack() }}
         style={{
@@ -44,8 +91,16 @@ const InstructorDetails = ({ route }) => {
       </View>
       <Text style={styles.instructorTitle}>{name}</Text>
       
-      <Text style={{color: "white"}}>Insert the details/reviews tab here</Text>
-      {/* https://reactnavigation.org/docs/tab-view/ */}
+      <View style={{ flex: 1, flexDirection: 'row', backgroundColor: "black" }}>
+        <TabView
+          lazy
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: windowWidth, height: windowHeight / 2 }}
+          renderTabBar={props => <TabBar {...props} style={{ backgroundColor: 'black'}} indicatorStyle={{ backgroundColor: '#35C2C1' }} />}
+        />
+      </View>
       {loading && <Loader />}
     </View>
   );
