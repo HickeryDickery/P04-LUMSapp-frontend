ea926b841c1f07284083637ecc22e59dda64ede6
Owais Ahsan <owais.ahsan@hotmail.com>
2024-03-25 00:05:23 +0500

    Add Notifications tab

--- constants/bottomtabs.ts
+++ constants/bottomtabs.ts
@@ -3,21 +3,22 @@ import LdfHomePage from "../screens/LdfHomePage";
 import AddPost from "../screens/AddPost";
 import HomeStack from "../components/HomeStack";
 import Profile from "../screens/Profile";
+import Notifications from "../screens/NotificationsTab";
 import EventTopTabs from "../components/EventTopTabs";
 
 type BottomTab = {
-  name: string;
-  icon: keyof typeof Ionicons.glyphMap;
-  component: React.FC;
+    name: string;
+    icon: keyof typeof Ionicons.glyphMap;
+    component: React.FC;
 };
 
 const bottomTabs: BottomTab[] = [
-  { name: "LDF", icon: "people-circle", component: LdfHomePage },
-  { name: "AllEvents", icon: "calendar", component: EventTopTabs },
-  { name: "HomeStack", icon: "home", component: HomeStack },
-  { name: "AddPost", icon: "add-circle", component: AddPost },
-  // { name: "Donations", icon: "fitness" },
-  { name: "Profile", icon: "person", component: Profile },
+    { name: "LDF", icon: "people-circle", component: LdfHomePage },
+    { name: "AllEvents", icon: "calendar", component: EventTopTabs },
+    { name: "HomeStack", icon: "home", component: HomeStack },
+    { name: "AddPost", icon: "notifications", component: Notifications },
+    // { name: "Donations", icon: "fitness" },
+    { name: "Profile", icon: "person", component: Profile },
 ];
 
 export default bottomTabs;
