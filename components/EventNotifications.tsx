88c6183a2904468adfcafa2310bf9a61df6e8233
Owais Ahsan <owais.ahsan@hotmail.com>
2024-03-27 23:36:51 +0500

    Event Notifications fix

--- components/EventNotifications.tsx
+++ components/EventNotifications.tsx
@@ -86,7 +86,7 @@ const EventNotifications = (props: any) => {
                                         paddingHorizontal: 10,
                                     }}
                                 >
-                                    {item.society}
+                                    {item.venue}
                                 </Text>
                                 <Text
                                     style={{
