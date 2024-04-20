0bd663c9cc810584628e9822ecd81b9c90f38be5
Owais Ahsan <owais.ahsan@hotmail.com>
2024-04-21 00:07:14 +0500

    Header fix

--- components/BottomTabs.tsx
+++ components/BottomTabs.tsx
@@ -8,38 +8,56 @@ import Header from "./Header";
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
-  return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Header />
-      <Tab.Navigator
-        initialRouteName="HomeStack"
-        screenOptions={({ route }) => ({
-          tabBarShowLabel: false,
-          tabBarStyle: { backgroundColor: "black" },
-          tabBarActiveTintColor: "#DDDDDD",
-          tabBarInactiveTintColor: "#35C2C1",
-          headerShown: false,
-        })}
-      >
-        {bottomTabs.map((tab) => (
-          <Tab.Screen
-            key={tab.name}
-            name={tab.name}
-            component={tab.component}
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <Ionicons
-                  name={tab.icon}
-                  size={focused ? 24 : 20}
-                  color={focused ? "#DDDDDD" : "#35C2C1"}
-                />
-              ),
-            }}
-          />
-        ))}
-      </Tab.Navigator>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+            <Tab.Navigator
+                initialRouteName="HomeStack"
+                screenOptions={({ route }) => ({
+                    tabBarShowLabel: false,
+                    tabBarStyle: { backgroundColor: "black" },
+                    tabBarActiveTintColor: "#DDDDDD",
+                    tabBarInactiveTintColor: "#35C2C1",
+                    headerShown: route.name == "Profile" ? false : true,
+                    headerBackground(props) {
+                        return (
+                            <View
+                                style={{
+                                    backgroundColor: "black",
+                                    height: 50,
+                                    width: "100%",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <Header />
+                            </View>
+                        );
+                    },
+                    headerStyle: {
+                        backgroundColor: "black",
+                        height: 50,
+                    },
+                })}
+            >
+                {bottomTabs.map((tab) => (
+                    <Tab.Screen
+                        key={tab.name}
+                        name={tab.name}
+                        component={tab.component}
+                        options={{
+                            headerTitle: "",
+                            tabBarIcon: ({ focused }) => (
+                                <Ionicons
+                                    name={tab.icon}
+                                    size={focused ? 24 : 20}
+                                    color={focused ? "#DDDDDD" : "#35C2C1"}
+                                />
+                            ),
+                        }}
+                    />
+                ))}
+            </Tab.Navigator>
+        </View>
+    );
 };
 
 export default BottomTabs;
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,24 +3,28 @@ import { Avatar } from "react-native-paper";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const Header = () => {
-  return (
-    <View style={styles.header}>
-      <Avatar.Image size={30} source={require("../assets/adaptive-icon.png")} />
-      <Image source={require("../assets/logo.png")} />
-      <MaterialIcons name="message" size={24} color="#35C2B0" />
-    </View>
-  );
+    return (
+        // <View style={styles.header}>
+        //   <Avatar.Image size={30} source={require("../assets/adaptive-icon.png")} />
+        //   <Image source={require("../assets/logo.png")} />
+        //   <MaterialIcons name="message" size={24} color="#35C2B0" />
+        // </View>
+        <Image
+            style={{ width: 50, height: 50 }}
+            source={require("../assets/logo.png")}
+        />
+    );
 };
 
 export default Header;
 
 const styles = StyleSheet.create({
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 20,
+    },
 });
