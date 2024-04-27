0c697a712627bd2cab272b0594cb1a8f14a010b0
Hamza Akmal <24100225@lums.edu.pk>
2024-04-27 19:44:24 +0500

    Latest update

--- screens/InstructorDetails.tsx
+++ screens/InstructorDetails.tsx
@@ -1,12 +1,21 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, Dimensions, Image, ScrollView, Keyboard, TouchableWithoutFeedback} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { Button } from "react-native-paper";
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 import { Avatar } from "react-native-paper";
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import Loader from "../components/Loader";
 import axios from "axios";
 import { IP } from "../constants/ip";
@@ -18,36 +27,90 @@ import Reviews from "../components/Reviews";
 const profilepicture: string = "../assets/adaptive-icon.png";
 
 // tabview 1
-const DetailsTab = (extraProp:any) => {
-  const { reviewRating, reviewsCount, zambeelRating, profileDescription } = extraProp.extraProp;
+const DetailsTab = (extraProp: any) => {
+  const { reviewRating, reviewsCount, zambeelRating, profileDescription } =
+    extraProp.extraProp;
   return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View style={{ flex: 1}}>
-          <Text style={{color: "#35C2C1", fontSize: 15, fontWeight: "bold", padding: 20}}>Overall Rating</Text>
-        
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <View style={{paddingLeft: 20}}><StarRating initialValue={reviewRating}/></View>
-            <Text style={{ color: 'white', marginLeft: 5, marginTop: 10, fontSize: 10}}>({reviewsCount})</Text>
-            <Text  style={{ color: 'white', marginLeft: zambeelRating === 0 ? 100 : 80, fontSize: 25, fontWeight: "bold"}}>{zambeelRating === 0 ? "NA" : `${zambeelRating}/5`}</Text>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Text
+          style={{
+            color: "#35C2C1",
+            fontSize: 15,
+            fontWeight: "bold",
+            padding: 20,
+          }}
+        >
+          Overall Rating
+        </Text>
+
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={{ paddingLeft: 20 }}>
+            <StarRating initialValue={reviewRating} />
           </View>
-          <Text  style={{ color: '#047CD2', marginLeft: 300, fontSize: 15}}>Zambeel</Text>
-         
-          <Text style={{color: "#35C2C1", fontSize: 15, fontWeight: "bold", padding: 20}}>Profile</Text>
-          <Text style={{color: "white", fontSize: 15, paddingHorizontal: 20}}>{profileDescription}</Text>
+          <Text
+            style={{
+              color: "white",
+              marginLeft: 5,
+              marginTop: 10,
+              fontSize: 10,
+            }}
+          >
+            ({reviewsCount})
+          </Text>
+          <Text
+            style={{
+              color: "white",
+              marginLeft: zambeelRating === 0 ? 100 : 80,
+              fontSize: 25,
+              fontWeight: "bold",
+            }}
+          >
+            {zambeelRating === 0 ? "NA" : `${zambeelRating}/5`}
+          </Text>
         </View>
-      </ScrollView>
-)
-  };
+        <Text style={{ color: "#047CD2", marginLeft: 300, fontSize: 15 }}>
+          Zambeel
+        </Text>
+
+        <Text
+          style={{
+            color: "#35C2C1",
+            fontSize: 15,
+            fontWeight: "bold",
+            padding: 20,
+          }}
+        >
+          Profile
+        </Text>
+        <Text style={{ color: "white", fontSize: 15, paddingHorizontal: 20 }}>
+          {profileDescription}
+        </Text>
+      </View>
+    </ScrollView>
+  );
+};
 
 // tabview 2
 const ReviewsTab = (extraProp: any) => {
   const navigation = useNavigation();
-  const { reviewRating, reviewsCount, zambeelRating, profileDescription, reviews, name, instructorImage, userID, userImageURL, onDeleteSuccess } = extraProp.extraProp;
+  const {
+    reviewRating,
+    reviewsCount,
+    zambeelRating,
+    profileDescription,
+    reviews,
+    name,
+    instructorImage,
+    userID,
+    userImageURL,
+    onDeleteSuccess,
+  } = extraProp.extraProp;
   // Render item function for FlatList
   const renderItem = ({ item }: any) => {
     return (
-      <Reviews 
-        username={item.username} 
+      <Reviews
+        username={item.username}
         profilePicture={item.profilePicture}
         ratingGiven={item.ratingGiven}
         reviewDescription={item.reviewDescription}
@@ -61,32 +124,89 @@ const ReviewsTab = (extraProp: any) => {
 
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Avatar.Image style={{marginLeft: 20, marginTop: 10}} size={30} source={{uri: userImageURL}} />
-        <TouchableOpacity onPress={() => {navigation.navigate("AddInstructorReview", {name: name, instructorImage: instructorImage})}}> 
-          <View style={{marginLeft: 5, marginTop:10}}><StarRating initialValue={reviewRating}/></View>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Avatar.Image
+          style={{ marginLeft: 20, marginTop: 10 }}
+          size={30}
+          source={{ uri: userImageURL }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("AddInstructorReview", {
+              name: name,
+              instructorImage: instructorImage,
+            });
+          }}
+        >
+          <View style={{ marginLeft: 5, marginTop: 10 }}>
+            <StarRating initialValue={reviewRating} />
+          </View>
         </TouchableOpacity>
       </View>
 
-      <View style={{padding:5}}></View>
+      <View style={{ padding: 5 }}></View>
       <FlatList
         data={reviews}
         renderItem={renderItem}
-        keyExtractor={item => item._id.toString()} 
+        keyExtractor={(item) => item._id.toString()}
         contentContainerStyle={{ flexGrow: 1 }}
       />
     </View>
   );
-}; 
+};
 
 // tab changer for details and reviews
-const renderScene = ({ route, reviewRating, reviewsCount, zambeelRating, 
-    profileDescription, reviews, name, instructorImage, userID, userImageURL, onDeleteSuccess }: { route: any, reviewRating: number, reviewsCount: number, zambeelRating: number, profileDescription: string, reviews: any, name: string, instructorImage: string, userID: string, userImageURL:string, onDeleteSuccess:any }) => {
+const renderScene = ({
+  route,
+  reviewRating,
+  reviewsCount,
+  zambeelRating,
+  profileDescription,
+  reviews,
+  name,
+  instructorImage,
+  userID,
+  userImageURL,
+  onDeleteSuccess,
+}: {
+  route: any;
+  reviewRating: number;
+  reviewsCount: number;
+  zambeelRating: number;
+  profileDescription: string;
+  reviews: any;
+  name: string;
+  instructorImage: string;
+  userID: string;
+  userImageURL: string;
+  onDeleteSuccess: any;
+}) => {
   switch (route.key) {
-    case 'first':
-      return <DetailsTab extraProp={{reviewRating, reviewsCount, zambeelRating, profileDescription}} />;
-    case 'second':
-      return <ReviewsTab extraProp={{reviewRating, reviews, name, instructorImage, userID, userImageURL, onDeleteSuccess}} />;
+    case "first":
+      return (
+        <DetailsTab
+          extraProp={{
+            reviewRating,
+            reviewsCount,
+            zambeelRating,
+            profileDescription,
+          }}
+        />
+      );
+    case "second":
+      return (
+        <ReviewsTab
+          extraProp={{
+            reviewRating,
+            reviews,
+            name,
+            instructorImage,
+            userID,
+            userImageURL,
+            onDeleteSuccess,
+          }}
+        />
+      );
     default:
       return null;
   }
@@ -95,12 +215,14 @@ const renderScene = ({ route, reviewRating, reviewsCount, zambeelRating,
 const InstructorDetails = ({ route }: any) => {
   const navigation = useNavigation();
   const [loading, setLoading] = useState(false);
-  const windowWidth = Dimensions.get('window').width;
-  const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
   const { name, school, department } = route.params;
 
   // instructor details
-  const [instructorImage, setInstructorImage] = useState("https://picsum.photos/202");
+  const [instructorImage, setInstructorImage] = useState(
+    "https://picsum.photos/202"
+  );
   const [reviewsCount, setReviewsCount] = useState(0);
   const [reviewRating, setReviewRating] = useState(0);
   const [zambeelRating, setZambeelRating] = useState(0);
@@ -121,7 +243,12 @@ const InstructorDetails = ({ route }: any) => {
         body: name,
       });
       if (res.data.success && res.data.instructorInformation) {
-        const { instructorInformation, userID, requesteduserImageURL, reviewsInformation } = res.data;
+        const {
+          instructorInformation,
+          userID,
+          requesteduserImageURL,
+          reviewsInformation,
+        } = res.data;
         setInstructorImage(instructorInformation.instructorImage);
         setProfileDescription(instructorInformation.profileDescription);
         setReviewsCount(instructorInformation.reviewCount);
@@ -140,7 +267,8 @@ const InstructorDetails = ({ route }: any) => {
 
   // runs once
   useEffect(() => {
-    const timer = setTimeout(() => {  // wait for data to come
+    const timer = setTimeout(() => {
+      // wait for data to come
       setTimeup(true);
     }, 3500);
 
@@ -148,7 +276,8 @@ const InstructorDetails = ({ route }: any) => {
     return () => clearTimeout(timer);
   }, []);
 
-  const onDeleteSuccess = () => {  // callback function to refresh reviews
+  const onDeleteSuccess = () => {
+    // callback function to refresh reviews
     fetchData();
   };
 
@@ -166,44 +295,70 @@ const InstructorDetails = ({ route }: any) => {
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'first', title: 'Details' },
-    { key: 'second', title: 'Reviews' },
-  ]); 
+    { key: "first", title: "Details" },
+    { key: "second", title: "Reviews" },
+  ]);
   return profileDescription === null ? (
-    timeup ? (  // if both timeup and data is null, show coming soon
-      <View style={[styles.container, {justifyContent: 'center'}]}>
+    timeup ? ( // if both timeup and data is null, show coming soon
+      <View style={[styles.container, { justifyContent: "center" }]}>
         <Text style={styles.comingSoon}>Coming soon</Text>
       </View>
-    ) : (  // if timeup is false but data is null, show loader
+    ) : (
+      // if timeup is false but data is null, show loader
       <Loader />
     )
-  ) : (  // if data exits show the data
-  <TouchableWithoutFeedback onPress={handleKeyboardDismiss}>
-    <View style={styles.container}>
+  ) : (
+    // if data exits show the data
+    <TouchableWithoutFeedback onPress={handleKeyboardDismiss}>
+      <ScrollView style={styles.container}>
+        <View>
           <View>
-            <View>
-              <Image style={{ width: windowWidth, height: windowHeight / 3 }} source={{ uri: instructorImage }} />
-            </View>
-            <LinearGradient
-              colors={['transparent', 'rgba(0,0,0,1.0)']}
-              style={{ position: 'absolute', width: '100%', height: '100%' }}
+            <Image
+              style={{ width: windowWidth, height: windowHeight / 2 }}
+              source={{ uri: instructorImage }}
             />
           </View>
-          <Text style={styles.instructorTitle}>{name}</Text>
+          <LinearGradient
+            colors={["transparent", "rgba(0,0,0,1.0)"]}
+            style={{ position: "absolute", width: "100%", height: "100%" }}
+          />
+        </View>
+        <Text style={styles.instructorTitle}>{name}</Text>
 
-          <View style={{ flex: 1, flexDirection: 'row', backgroundColor: "black" }}>
-            <TabView
-              lazy
-              navigationState={{ index, routes }}
-              renderScene={({ route }) => renderScene({ route, reviewRating, reviewsCount, zambeelRating,
-                profileDescription, reviews, name, instructorImage, userID, userImageURL, onDeleteSuccess })}
-              onIndexChange={setIndex}
-              initialLayout={{ width: windowWidth, height: windowHeight / 2 }}
-              renderTabBar={props => <TabBar {...props} style={{ backgroundColor: 'black' }} indicatorStyle={{ backgroundColor: '#35C2C1' }} />}
-            />
-          </View>
-      {loading && <Loader />}
-    </View>
+        <View
+          style={{ flex: 1, flexDirection: "row", backgroundColor: "black" }}
+        >
+          <TabView
+            lazy
+            navigationState={{ index, routes }}
+            renderScene={({ route }) =>
+              renderScene({
+                route,
+                reviewRating,
+                reviewsCount,
+                zambeelRating,
+                profileDescription,
+                reviews,
+                name,
+                instructorImage,
+                userID,
+                userImageURL,
+                onDeleteSuccess,
+              })
+            }
+            onIndexChange={setIndex}
+            initialLayout={{ width: windowWidth, height: windowHeight / 2 }}
+            renderTabBar={(props) => (
+              <TabBar
+                {...props}
+                style={{ backgroundColor: "black" }}
+                indicatorStyle={{ backgroundColor: "#35C2C1" }}
+              />
+            )}
+          />
+        </View>
+        {loading && <Loader />}
+      </ScrollView>
     </TouchableWithoutFeedback>
   );
 };
@@ -219,17 +374,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
-    textAlignVertical: 'top',
+    textAlignVertical: "top",
     marginTop: 50,
     marginBottom: 20,
   },
   instructorTitle: {
-    color: "white", 
+    color: "white",
     fontWeight: "bold",
     fontSize: 20,
     position: "absolute",
     left: "9%",
-    top: "43%"
+    top: "43%",
   },
   comingSoon: {
     backgroundColor: "#000",
@@ -239,5 +394,3 @@ const styles = StyleSheet.create({
 });
 
 export default InstructorDetails;
-
-
